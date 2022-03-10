4d83da7ca910f7c2eb405545ce2b4dfb4ce97563
Emerson Freire <emersonnascimento.freire@gmail.com>
2022-03-09 22:28:24 -0300

    Adicionado novas funções e opções

--- decida_por_mim.py
+++ decida_por_mim.py
@@ -15,36 +15,66 @@ Habilidades praticas a aplicar:
 #Tente adicionar uma que pergunte a data.
 #Tente adicionar uma, que fale problemas matematicos....
 #adicionar interface
+import datetime
 import random
 from tkinter import *
 from tkinter import messagebox
 import time
 def oquefazer():
     listaresposta = ['Treinar Python e se alimentar direito', 'Assistir um programa divertido',
-             'Já se hidratou hoje? Beba água','Leia um livro', 'Dá uma espiada no Insta',
+             'Já se hidratou hoje? Beba água.','Leia um livro.', 'Dá uma espiada no Insta.',
              'Faça sua tarefa que está acumulada!', 'Vai lavar a louça!',
              'Não sei, sou apenas um computador!', 'Faça o que quiser, você é livre :D',
              'Não entre em pânico!', 'Nada, tire o dia para descansar', 'Comece uma dieta!',
              'Leve o cachorro para passear.', 'Vá dormir', 'Que tal um filme?',
-             'Meditação pode ajudar', 'Boa pergunta!']
+             'Meditação pode ajudar', 'Boa pergunta!', 'Comece algo novo!']
 
     resposta = random.choice(listaresposta)
-    texto_resposta['text'] = resposta
+    fazer_resposta['text'] = resposta
 
 def horas():
-    horario= time.clock()
+    horas = datetime.datetime.now().hour
+    min = datetime.datetime.now().minute
+    sec = datetime.datetime.now().second
+    horario = [f'{horas}:{min}:{sec}']
+    horas_resposta['text'] = horario
 
+def comida():
+    listacomida = ['pizza.', 'Hamburguer.', 'Já almoçou? Come um doce!',
+                   'Algo saudável, como um MC Donalds', 'Nada! Você já comeu demais',
+                   'Beba água', 'Churrasco!', 'Arroz e feijão','Macarrão vai bem!',
+                   'Um suco de laranja talvez?', 'Come um doce e se arrependa',
+                   'Pão com ovo', 'Ração de gato? Não sei o que humanos comem',
+                   'Desculpe, sem ideias','Comida japonesa!', 'Comida mexicana!',
+                   'Um omelete com queijo', 'Mas já tá com fome???', 'Nada, regime já!',
+                   'Uma salada', 'Vocês comem oquê?', 'Um terabyte de dados??',
+                   'Adeus e obrigado pelos peixes!', 'Frango assado', 'Bolo de chocolate']
+    randomiza = random.choice(listacomida)
+    comida_randomiza['text']=randomiza
 #Área da interface
 quadro = Tk()
 quadro.title('Decida por mim')
-quadro.geometry('300x500')
+quadro.geometry('300x300')
 
-botao_fazer = Button(quadro,sleep(1),text='O que devo fazer?'
+#função (O que fazer)
+botao_fazer = Button(quadro,text='O que devo fazer?'
                      ,bg='#00f100', command=oquefazer)
-botao_fazer.grid(column=0, row=0, padx=70, pady=30)
+botao_fazer.grid(column=0, row=0, padx=70, pady=10)
+fazer_resposta = Label(quadro,text='')
+fazer_resposta.grid(column=0, row=1)
 
-texto_resposta = Label(quadro,text='')
-texto_resposta.grid(column=0, row=2)
+#função (Horas)
+botao_horas = Button(quadro,text='Me diga as horas',
+                     bg='#f000f0', command=horas)
+botao_horas.grid(column=0, row=2, padx=70, pady=10)
+horas_resposta = Label(quadro,text='')
+horas_resposta.grid(column=0, row=3)
+
+#função (comida)
+botao_comida = Button(quadro,text='O que devo comer?',bg='#ffff00', command=comida)
+botao_comida.grid(column=0, row=4)
+comida_randomiza = Label(quadro,text='')
+comida_randomiza.grid(column=0, row=5)
 
 quadro.mainloop()
 
